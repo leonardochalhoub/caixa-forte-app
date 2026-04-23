041d150374ef12a3f6ea76cb4c78acfe41896e17
Leonardo Chalhoub <leonardo.chalhoub-ext@ab-inbev.com>
2026-04-23 16:56:09 -0300

    chore: remove // a marker

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -8,7 +8,6 @@ import { PrivacyDisclaimer } from "@/components/PrivacyDisclaimer"
 import { SafeBoxIcon } from "@/components/SafeBoxIcon"
 import { ThemeToggle } from "@/components/theme-toggle"
 
-// a
 export default function LandingPage() {
   return (
     <div className="flex min-h-screen flex-col bg-canvas">
